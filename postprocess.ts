98d52de3b32bac7568a7ec2d11ebda50e0a27ba9
Blair Drummond <blair.drummond@canada.ca>
2021-08-25 10:42:38 -0400

    fix: grab sheet 0

--- postprocess.ts
+++ postprocess.ts
@@ -9,7 +9,7 @@ const outputFilename = inputFilename.replace(".xlsx", ".csv");
 // read about what the xlsx library can do here: https://github.com/SheetJS/sheetjs
 
 const workbook = await readXLSX(inputFilename);
-const sheetData = workbook.Sheets[workbook.SheetNames[2]];
+const sheetData = workbook.Sheets[workbook.SheetNames[0]];
 const csvString = await xlsx.utils.sheet_to_csv(sheetData); // can use to_json, to_txt, to_html, to_formulae
 
 // write to csv
