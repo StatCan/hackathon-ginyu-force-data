7c3ef7d34d3b10a5724bc2a2f8adae93e166787d
Blair Drummond <blair.drummond@canada.ca>
2021-08-27 16:14:28 -0400

    remove unused group_bys

--- .github/scripts/postprocess.py
+++ .github/scripts/postprocess.py
@@ -18,19 +18,19 @@ OUTFILE="data/estma_gb_3.csv"
 if not exists(ESTMA_PATH):
     sys.exit(0)
 
-makedirs('data', exist_ok=True)
-
-sample_estma = pd.read_csv(ESTMA_PATH, thousands=",")
-
-component_17 = sample_estma[["entity", "country", "period_start_date", "payment_category", "amount_reported_cad"]]
-component_17["count"] = 1
-
-component_17 = component_17.groupby(["entity", "country", "period_start_date", "payment_category"]).agg({
-    "count": "count",
-    "amount_reported_cad": "sum"
-}).reset_index()
-
-component_17.to_csv(OUTFILE)
+#makedirs('data', exist_ok=True)
+#
+#sample_estma = pd.read_csv(ESTMA_PATH, thousands=",")
+#
+#component_17 = sample_estma[["entity", "country", "period_start_date", "payment_category", "amount_reported_cad"]]
+#component_17["count"] = 1
+#
+#component_17 = component_17.groupby(["entity", "country", "period_start_date", "payment_category"]).agg({
+#    "count": "count",
+#    "amount_reported_cad": "sum"
+#}).reset_index()
+#
+#component_17.to_csv(OUTFILE)
 
 
 ###############################
